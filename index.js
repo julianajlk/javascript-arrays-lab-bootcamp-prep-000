4a021a1f3bd1f08ce19bdf8a8f00445b4a635280
Juliana Kang <julianajlk@gmail.com>
2018-05-10 23:41:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(kitten) {
-  [...kittens, 'Ralph'];
+  kittens.push(kitten);
   return kittens;
 }
