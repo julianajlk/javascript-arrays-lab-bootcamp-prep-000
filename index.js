ec255e3ee20bddacfa53050dea0933793fd20142
Juliana Kang <julianajlk@gmail.com>
2018-05-10 23:49:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ function destructivelyAppendKitten(kitten) {
   kittens.push(kitten);
   return kittens;
 }
+
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten);
+  return kittens;
+}
